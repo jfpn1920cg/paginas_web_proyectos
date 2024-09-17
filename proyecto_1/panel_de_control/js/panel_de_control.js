19dc019fc5e607ad80eee6fcf9f1875fd1743a14
jfpn1920cr <162055100+jfpn1920cg@users.noreply.github.com>
2024-09-17 15:44:59 -0500

    agregando_nuevo_contenido

--- proyecto_1/panel_de_control/js/panel_de_control.js
+++ proyecto_1/panel_de_control/js/panel_de_control.js
@@ -1,11 +1,44 @@
-//funacionalidad_menu_de_navegacion
-document.addEventListener('DOMContentLoaded', function() {
-    const menu_de_navegacion_desplegable = document.getElementById('menu_de_navegacion_desplegable');
-    const menu_de_navegacion = document.getElementById('menu_de_navegacion');
+// script.js
 
-    if (menu_de_navegacion_desplegable && menu_de_navegacion) {
-        menu_de_navegacion_desplegable.addEventListener('click', () => {
-            menu_de_navegacion.classList.toggle('active');
-        });
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Función para cambiar diapositiva
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Función para establecer la diapositiva actual
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// Función para mostrar diapositivas
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-});
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Funciones para controles prev y next
+function prevSlide() {
+    plusSlides(-1);
+}
+
+function nextSlide() {
+    plusSlides(1);
+}
